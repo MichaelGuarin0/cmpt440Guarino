3082e65b7c79c3740fa671d8d6d74a555db4696d
MichaelGuarin0 <michael.guarino1@marist.edu>
2017-03-15 21:04:31 -0400

    completed homework 1 with comments and formatted according to style guide

--- hw/homework1/ManWolf.java
+++ hw/homework1/ManWolf.java
@@ -1,56 +1,144 @@
-/*
-@author: Michael Guarino
-professor: Pablo Rivas
-class: cmpt_440
-desc: file containing the abstract class of the man-wolf-goat-cabbage problem, i.e., class definition, all its attributes, all its methods
-notes:
-Instructions: Using a table-driven DFA approach, write a Java class ManWolf that takes a string from the command line and reports whether or not it represents a solution to the man-wolf-goat-cabbage problem of Chapter 2 in the textbook. You will implement your ManWolf class (ManWolf.java) in a driver java file named driverDFA.java that contains only the part that reads from standard input, calls the functions of the ManWolf class, and prints the result to standard output. For example, it should have this exact same behavior:
-   > java driverDFA gncgwng
-   That is a solution.
-   > java driverDFA ggggggggg
-   That is not a solution.
-*/
+/**
+  * file: ManWolf.java
+  * author: Michael Guarino
+  * course: CMPT 440
+  * assignment: homework 1
+  * due date: March 20, 2017
+  * version: completed
+  *
+  * This file contains declaration of the
+  * abstract class ManWolf.
+  */
 
+import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
-//takes a string from the command line and reports whether or not it represents a solution to the man-wolf-goat-cabbage problem
-public class ManWolf {
-  //reads in csv and creates grade translation table
+/**
+  * ManWolf
+  * 
+  * The abstract class ManWolf contains all necessary methods
+  * to recieve an input string and determines if that string is
+  * a valid solution to the man-wolf-goat-cabbage problem.
+  */
+public abstract class ManWolf{
+
+  /**
+  * validateInput
+  *
+  * This static method takes a string from the command line
+  * and determines if the characters in that string
+  * are valid input commands (nwgc) in the
+  * man-wolf-goat-cabbage problem.
+  *
+  * Parameters:
+  *   args: a string of inputs taken from the command line
+  *
+  * Return value: boolean value indicating that the input string
+  * is valid/invalid input (not determining if input is a valid/invalid solution)
+  */
+  public static boolean validateInput(String args){
+    return args.matches("[nwgc]+");
+  }
+
+  /**
+  * createTransTable
+  *
+  * This helper method to process creates a 2D table from the stateTransitions csv file
+  * that defines all possible state transitions for given input.
+  *
+  * Parameters: none
+  *
+  * Return value: transTable that defines all possible state transitions.
+  */
   private int[][] createTransTable() throws IOException{
-    //open file input stream
     BufferedReader reader = new BufferedReader(new FileReader("stateTransitions.csv"));
-    String line = null;
     Scanner scanner = null;
+    String line = null;
     int rowIndex = 0;
     int colIndex = 0;
     int[][] transTable = new int[11][4];
     //read file line by line
     while((line = reader.readLine()) != null){
-        scanner = new Scanner(line);
-        scanner.useDelimiter(",");
-        while(scanner.hasNext()){
-            int data = scanner.nextInt();
-            if(colIndex == 0)
-                transTable[rowIndex][colIndex] = data;
-            else if(colIndex == 1)
-                transTable[rowIndex][colIndex] = data;
-            else if(colIndex == 2)
-                transTable[rowIndex][colIndex] = data;
-            else if(colIndex == 3)
-                transTable[rowIndex][colIndex] = data;
-            colIndex ++;
+      scanner = new Scanner(line);
+      scanner.useDelimiter(",");
+      while(scanner.hasNext()){
+        //insert data from csv into transTable
+        int data = scanner.nextInt();
+        if(colIndex == 0){
+          transTable[rowIndex][colIndex] = data;
         }
-        colIndex = 0;
-        rowIndex ++;
+        else if(colIndex == 1){
+          transTable[rowIndex][colIndex] = data;
+        }
+        else if(colIndex == 2){
+          transTable[rowIndex][colIndex] = data;
+        }
+        else if(colIndex == 3){
+          transTable[rowIndex][colIndex] = data;
+        }
+        colIndex ++;
+      }
+      colIndex = 0;
+      rowIndex ++;
     }
     reader.close();
     return transTable;
   }
 
-  //makes one transition for each character recieved in a given input string
+  /**
+  * translateInput
+  *
+  * This helper method to process takes an input string, converts the character
+  * of the input string defined as commands in the man-wolf-goat-cabbage
+  * and converts them to numeric string values used later to index into
+  * the transTable.
+  *
+  * Parameters:
+  *   input: string of input values that correspond to state transitions
+  *
+  * Return value: string[] numeric string values translated from corresponding character values from input string
+  */
+  private String[] translateInput(String input){
+    String[] splitInput = input.replaceAll("n", Integer.toString(0)).replaceAll("w", Integer.toString(1)).replaceAll("g", Integer.toString(2)).replaceAll("c", Integer.toString(3)).split("");
+    return splitInput;
+  }
+
+  /**
+  * accepted
+  *
+  * This helper method to process determines if the
+  * input string is a valid solution to the man-wolf-goat-cabbage
+  * problem.
+  *
+  * Parameters:
+  *   state: integer corresponding to the final state after all
+  *   state transition have been performed as defined by the input string
+  *
+  * Return value: boolean value that determines if acceptance state has
+  * been reached
+  */
+  private boolean accepted(int state){
+    int acceptanceState = 9;
+    return state == acceptanceState;
+  }
+
+  /**
+  * process
+  *
+  * This function makes recieves an input string and
+  * makes one state transition for every for each
+  * character in the input string. When all state
+  * transitions are completed this function uses the
+  * accepted helper function to determine if the input
+  * string was valid input.
+  *
+  * Parameters:
+  *   input: string of input values that correspond to state transitions
+  *
+  * Return value: boolean value that flags input value as a valid/invalid solution.
+  */
   public boolean process(String input){
     //create transition table from csv
     int[][] transTable = null;
@@ -58,31 +146,17 @@ public class ManWolf {
       transTable = createTransTable();
     }
     catch(IOException e){
-        System.out.println(e.getMessage());
+      System.out.println(e.getMessage());
     }
+    //translate input into numeric string array
     String[] transInput = translateInput(input);
-    int state = 0;
+    //reset state to initial state
+    int initialState = 0;
+    int state = initialState;
+    //perform state transitions
     for(int count = 0; count < transInput.length; count++){
       state = transTable[state][Integer.parseInt(transInput[count])];
     }
     return accepted(state);
-  }
-
-  //helper function process to tranlate input string into format accepted by grade translation table
-  private String[] translateInput(String input){
-    //translate to correct column from char
-    String[] splitInput = input.replaceAll("n", Integer.toString(0)).replaceAll("w", Integer.toString(1)).replaceAll("g", Integer.toString(2)).replaceAll("c", Integer.toString(3)).split("");
-    return splitInput;
-  }
-
-  //check if string was accepted
-  private boolean accepted(int state){
-    //takes in state then determines if ends in an accepting state
-    return state == 9;
-  }
-
-  //validate input from command line args
-  public static boolean validateInput(String args){
-        return args.matches("[nwgc]+");
   }
 }
--- hw/homework1/driverDFA.java
+++ hw/homework1/driverDFA.java
@@ -1,27 +1,55 @@
-/*
-@author: Michael Guarino
-professor: Pablo Rivas
-class: cmpt_440
-desc: file containing the implementation of the class above
-notes:
-*/
+/**
+  * file: driverDFA.java
+  * author: Michael Guarino
+  * course: CMPT 440
+  * assignment: homework 1
+  * due date: March 20, 2017
+  * version: completed
+  *
+  * This file contains declaration and implementation
+  * the driverDFA class.
+  */
 
-//contains only the part that reads from standard input, calls the functions of manwolf class
-//prints the result to standard output
 import java.io.IOException;
 
-public class driverDFA{
+/**
+  * driverDFA
+  * 
+  * This class implements the abstract class ManWolf.
+  * It takes input from the command line and uses methods
+  * from the ManWolf class implemented in the driverDFA class
+  * to determine if the command line input is a valid solution
+  * to the man-wolf-goat-cabbage problem.
+  */
+public class driverDFA extends ManWolf{
+
+  /**
+  * main
+  *
+  * The main method takes command line arguments
+  * and determines if they are valid input and 
+  * then calls methods of the driverDFA class
+  * to determine if command line input is a valid
+  * solution to man-wolf-goat-cabbage problem.
+  *
+  * Parameters:
+  *   args: command line argument tested as a valid solution
+  *   to man-wolf-goat-cabbage problem
+  *
+  * Return value: none
+  */
   public static void main(String[] args){
-    //is command line input valid
+    //determine if command line input is valid input
     if(args.length == 0){
       System.out.println("Please enter input.");
     }
-    else if(! ManWolf.validateInput(args[0])){
+    else if(! validateInput(args[0].toLowerCase())){
       System.out.println("This is not valid input.");
     }
+    //determine if command line input is valid solution
     else{
-      ManWolf test = new ManWolf();
-      if(test.process(args[0])){
+      driverDFA tester = new driverDFA();
+      if(tester.process(args[0].toLowerCase())){
         System.out.println("This is a solution.");
       }
       else{
