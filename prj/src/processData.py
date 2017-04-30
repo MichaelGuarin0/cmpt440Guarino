d9f74489594cf245f8f1892a0c679589afbc3744
MichaelGuarin0 <michael.guarino1@marist.edu>
2017-04-30 19:31:51 -0400

    data preprocessing almost completed

--- prj/src/processData.py
+++ prj/src/processData.py
@@ -1,5 +1,21 @@
-import nltk
+import re, os
+from nltk.tokenize import StanfordTokenizer
 
-test
+from utils import DATA_DIR
 
-asd;fkajsdf
+def _readDataFile(file):
+    with open(file) as data:
+        textData = data.read()
+    textData = re.sub('[.!?]\s+', '<eos>', textData)
+    textData = re.sub(',', '', textData)
+    return textData
+
+def dtm_builder():
+    dataFiles = ['{}/{}'.format(DATA_DIR, file) for file in os.listdir(DATA_DIR) if file.endswith('.txt')]
+    textData_allDocs = [_readDataFile(file) for file in dataFiles]
+    textData_sentences = [file.split('<eos>') for file in textData_allDocs]
+    print(textData_sentences)
+    #tokenizer =[ MosesTokenizer() for sentence in textData_sentences]
+
+#determine all unique words in all the documents so taht a one hot encoding can be constructed
+dtm_builder()
---
+++ prj/src/utils.py
@@ -0,0 +1,6 @@
+import os
+
+SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+PRJ_DIR = '/'.join(SRC_DIR.split('/')[:-1])
+LIB_DIR = '{}/{}'.format(PRJ_DIR, 'lib')
+DATA_DIR = '{}/{}'.format(LIB_DIR, 'data')
