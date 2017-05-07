649522e44eefe3355d23f47e1a3670fff7a9fdf2
MichaelGuarin0 <michael.guarino1@marist.edu>
2017-05-07 17:33:14 -0400

    completed project write up

--- prj/writeup/code/utils.py
+++ prj/writeup/code/utils.py
@@ -9,8 +9,7 @@ import os
 #project paths
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 PRJ_DIR = '/'.join(SRC_DIR.split('/')[:-1])
-LIB_DIR = '{}/{}'.format(PRJ_DIR, 'lib')
-DATA_DIR = '{}/{}'.format(LIB_DIR, 'data')
+DATA_DIR = '{}/{}'.format(PRJ_DIR, 'data')
 
 #project constants
 SEQUENCE_LENGTH = 30
